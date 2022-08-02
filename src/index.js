ba06938b6b6ed6287b683e88ad5cad8d0d68103e
wisdom obioma <wisdomobiomadev@gmail.com>
2022-08-02 13:07:34 +0100

    create and integrate useWorkoutContext

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { WorkoutContextProvider } from './context/WorkoutContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+	<React.StrictMode>
+		<WorkoutContextProvider>
+			<App />
+		</WorkoutContextProvider>
+	</React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
