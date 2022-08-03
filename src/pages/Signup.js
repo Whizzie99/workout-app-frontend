ef87a99308197a7a8351b5df624b33c4ae1ed0f0
wisdom obioma <wisdomobiomadev@gmail.com>
2022-08-03 01:20:10 +0100

    create useSignup hook

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,11 +1,15 @@
 import { useState } from 'react';
+import { useSignup } from '../hooks/useSignup';
 
 const Signup = () => {
 	const [email, setEmail] = useState('');
 	const [password, setPassword] = useState('');
+	const { signup, error, isLoading } = useSignup();
 
 	const handleSubmit = async (e) => {
 		e.preventDefault();
+
+		await signup(email, password);
 
 		console.log(email, password);
 	};
@@ -28,7 +32,8 @@ const Signup = () => {
 				value={password}
 			/>
 
-			<button>sign up</button>
+			<button disabled={isLoading}>sign up</button>
+			{error && <div className="error">{error}</div>}
 		</form>
 	);
 };
