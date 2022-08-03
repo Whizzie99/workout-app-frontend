7859d61dc23656d22602d201777a92a9a111607e
wisdom obioma <wisdomobiomadev@gmail.com>
2022-08-03 01:54:23 +0100

    create useLogin hook

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,10 @@
 import { Link } from 'react-router-dom';
 import { useLogout } from '../hooks/useLogout';
+import { useAuthContext } from '../hooks/useAuthContext';
 
 const Navbar = () => {
 	const { logout } = useLogout();
+	const { user } = useAuthContext();
 
 	const handleClick = () => {
 		logout();
@@ -15,13 +17,18 @@ const Navbar = () => {
 					<h1>workout buddy</h1>
 				</Link>
 				<nav>
-					<div>
-						<button onClick={handleClick}>logout</button>
-					</div>
-					<div>
-						<Link to="/login">login</Link>
-						<Link to="/signup">signup</Link>
-					</div>
+					{user && (
+						<div>
+							<span>{user.email}</span>
+							<button onClick={handleClick}>logout</button>
+						</div>
+					)}
+					{!user && (
+						<div>
+							<Link to="/login">login</Link>
+							<Link to="/signup">signup</Link>
+						</div>
+					)}
 				</nav>
 			</div>
 		</header>
